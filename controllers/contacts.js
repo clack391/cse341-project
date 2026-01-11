63b9a32ff08a6952160d7934bb8eaab734344ab2
Precious Onotu <clack391@gmail.com>
2026-01-11 14:05:15 +0100

    update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-    const result = await mongodb.getDb().db('project1').collection('users').find();
+    const result = await mongodb.getDb().db('project1').collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists);
@@ -11,7 +11,7 @@ const getAll = async (req, res, next) => {
 
 const getSingle = async (req, res, next) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db('project1').collection('users').find({ _id: userId });
+    const result = await mongodb.getDb().db('project1').collection('contacts').find({ _id: userId });
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists[0]);
