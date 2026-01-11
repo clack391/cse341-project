5e7075ead66e330aba0af78c43b78e266eae2b10
Precious Onotu <clack391@gmail.com>
2026-01-11 07:11:57 +0100

    feat: contacts api complete

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const mongodb = require('./db/connect');
+const port = 8080;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Connected to DB and listening on ${port}`);
+        });
+    }
+});
