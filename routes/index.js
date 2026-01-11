571faff7fc1432a333c4c31ecf961ca5f9725d9c
Precious Onotu <clack391@gmail.com>
2026-01-11 14:12:34 +0100

    Hello world

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ const router = require('express').Router();
 
 router.use('/contacts', require('./contacts'));
 
+router.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 module.exports = router;
